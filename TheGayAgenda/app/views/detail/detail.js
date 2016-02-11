0bc23ad2168cc45450699a13242cbddc76fec639
Leonardgrr <leonardgrr@fullsail.edu>
2016-02-11 16:10:07 -0500

    users can now do a simple check in - the userID along with the category and points will be written to the database

--- TheGayAgenda/app/views/detail/detail.js
+++ TheGayAgenda/app/views/detail/detail.js
@@ -9,6 +9,64 @@ angular.module('myApp.detail', ['ngRoute'])
   });
 }])
 
-.controller('detailCtrl', [function() {
+.controller('detailCtrl', [ '$rootScope', '$scope', '$firebaseAuth', '$firebaseObject', '$firebaseArray', '$route', 
+	function($rootScope, $scope, $firebaseAuth, $firebaseObject, $firebaseArray, $route, user, Auth) {
+	// var auth = $firebaseAuth(ref);
+	var ref = new Firebase("https://thegayagenda.firebaseio.com");
+	$scope.authObj = $firebaseAuth(ref);
+	$scope.users = $firebaseObject(new Firebase("https://thegayagenda.firebaseio.com/users/"));
+	$scope.checkedIn = $firebaseArray(new Firebase("https://thegayagenda.firebaseio.com/checkedIn"));
+	$scope.authObj.$onAuth(function(authData) {
+		$rootScope.authorize(authData);
+	});
+
+	$rootScope.authorize = function(authData){
+		if (authData) {
+		  	$scope.userData = authData;
+		  	var user = $firebaseObject(new Firebase("https://thegayagenda.firebaseio.com/users/"+authData.uid));
+		  	if (authData.provider === "google"){
+			  	user.profilePic = authData.google.profileImageURL;
+			  	user.userName = authData.google.displayName;
+			  	user.$save();
+		  	}
+		}
+		//  else {
+		//   	//if user not logged in
+		//   	$location.path('/');
+		// }
+	}
+
+	// This gets the data for the logged in user to CRUD
+	$scope.authObj.$onAuth(function(authData) {
+		$rootScope.authorize(authData);
+		if (authData) {
+		  	$scope.userData = authData;
+		  	// for use to use with current user crud ie: edit/delete user comments
+		  	$rootScope.currentUser = $scope.userData.uid;
+		  	$rootScope.currentUserDataAll = $scope.userData;
+		  	// $rootScope.currentUser1 = $scope.userData.uid;
+		  	// console.log("current user is ", $scope.currentUser);
+		  	// console.log($scope.currentUserDataAll);
+		  	// console.log("current user is ", $rootScope.currentUser1);
+		} 
+	});
+
+
+	// User Check In Function
+	$scope.newCheckIn = function(){
+		console.log("userId "+ $scope.userData.uid, "category "+$scope.newCategory.category, "points "+$scope.newPoints.points);
+		$scope.checkedIn.$add({
+			userId : $scope.userData.uid,
+			category: $scope.newCategory.category,
+			points: $scope.newPoints.points
+		})
+		// $scope.newCheckIn.userCheckIn = "";
+	}
+
+
+
+
+
+
 
 }]);
--- TheGayAgenda/app/views/profile/profile.js
+++ TheGayAgenda/app/views/profile/profile.js
@@ -9,6 +9,59 @@ angular.module('myApp.profile', ['ngRoute'])
   });
 }])
 
-.controller('profileCtrl', [function() {
+.controller('profileCtrl', [ '$rootScope', '$scope', '$firebaseAuth', '$firebaseObject', '$firebaseArray', '$route', 
+	function($rootScope, $scope, $firebaseAuth, $firebaseObject, $firebaseArray, $route, user, Auth) {
+	// var auth = $firebaseAuth(ref);
+	var ref = new Firebase("https://thegayagenda.firebaseio.com");
+	$scope.authObj = $firebaseAuth(ref);
+	$scope.users = $firebaseObject(new Firebase("https://thegayagenda.firebaseio.com/users/"));
+	$scope.checkIn = $firebaseArray(new Firebase("https://thegayagenda.firebaseio.com/checkIn"));
+	$scope.authObj.$onAuth(function(authData) {
+		$rootScope.authorize(authData);
+	});
+
+	$rootScope.authorize = function(authData){
+		if (authData) {
+		  	$scope.userData = authData;
+		  	var user = $firebaseObject(new Firebase("https://thegayagenda.firebaseio.com/users/"+authData.uid));
+		  	if (authData.provider === "google"){
+			  	user.profilePic = authData.google.profileImageURL;
+			  	user.userName = authData.google.displayName;
+			  	user.$save();
+		  	}
+		}
+		//  else {
+		//   	//if user not logged in
+		//   	$location.path('/');
+		// }
+	}
+
+
+
+	// User Check In Function
+	$scope.checkIn = function(){
+		console.log("did it check in?");
+		$scope.checkIn.$add({
+			userId : $scope.userData.uid,
+			picture: $scope.newCheckIn.userCheckIn,
+		})
+		$scope.newCheckIn.userCheckIn = "";
+	}
+
+
+	//user can save a link to their picture
+	// $scope.userAddPicture = function(){
+	// 	$scope.pictures.$add({
+	// 		userId : $scope.userData.uid,
+	// 		picture: $scope.newPicture.userPic
+	// 	})
+	// 	$scope.newPicture.userPic = "";
+	// }
+
+
+
+
+
+
 
 }]);
