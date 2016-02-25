52c2b26bf36a1034eace7239adab322f749be897
Leonardgrr <leonardgrr@fullsail.edu>
2016-02-25 13:48:06 -0500

    Fixed the card layout problem in listing.html with md-card edits in app.css

--- TheGayAgenda/app/views/admin/admin.js
+++ TheGayAgenda/app/views/admin/admin.js
@@ -42,9 +42,7 @@ angular.module('myApp.admin', ['ngRoute'])
 		15
     ];
 
-
-
-	//User Check In Function
+	//add new event and place function
 	$scope.neweventplace = function(){
 		console.log("Did it add something?");
 		$scope.eventplace.$add({
@@ -61,4 +59,65 @@ angular.module('myApp.admin', ['ngRoute'])
 		})
 	}
 
+
+	$scope.uploadImage = function(imageData, incidentID) {
+		console.log(imageData);
+    var FR = new FileReader();
+    var fb = new Firebase('https://thegayagenda.firebaseio.com/');
+    FR.onload = function(e) {
+    var imageString = e.target.result;
+
+	//create URL that refers to a specific event and add images as an array-like object to hold images
+	var eventReference = fb.child("events/" + incidentID);
+	var syncArray = $firebaseArray(eventReference.child("images"));
+
+      $scope.user = Auth.getUser();
+      var username = $scope.user.username || '';
+      var submitDate = new Date().toISOString().slice(0, 10);
+
+      syncArray.$add({
+          imageString: imageString,
+          username: username,
+          submitDate: submitDate
+	    })
+      .then(function() {
+          console.log('Image has been uploaded');
+      });
+    };
+    FR.readAsDataURL(imageData);
+  };
+
 }]);
+
+
+// angular.module('app.controllers', ['firebase'])
+
+// .controller('AppCtrl', function($scope,$firebaseArray) {
+//   $scope.uploadImage = function(imageData, incidentID) {
+//     var FR = new FileReader();
+
+//     FR.onload = function(e) {
+//       var imageString = e.target.result;
+
+// 	//create URL that refers to a specific event and add images as an array-like object to hold images
+//       var eventReference = fb.child(&quot;events/&quot; + incidentID);
+//       var syncArray = $firebaseArray(eventReference.child(&quot;images&quot;));
+
+//       $scope.user = Auth.getUser();
+//       var username = $scope.user.username || '';
+//       var submitDate = new Date().toISOString().slice(0, 10);
+
+//       syncArray.$add({
+//           imageString: imageString,
+//           username: username,
+//           submitDate: submitDate
+// 	    })
+//       .then(function() {
+//           console.log('Image has been uploaded');
+//       });
+//     };
+//     FR.readAsDataURL(imageData);
+//   });
+
+//   //...other code here...//
+// });
